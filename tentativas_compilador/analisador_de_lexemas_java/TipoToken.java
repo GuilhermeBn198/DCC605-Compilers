70ce6982e1cfc41076f502cd6ad5df9161cd9d01
Guilherme Bernardo <bguilherme51@gmail.com>
2023-03-21 20:42:01 -0400

    limpando repo

--- tentativas_compilador/analisador_de_lexemas_java/TipoToken.java
+++
@@ -1,7 +0,0 @@
-package analisador_de_lexemas_java;
-
-public enum TipoToken {
-	PCDeclaracoes, PCAlgoritmo, PCReal, PCInteiro, PCAtribuir, PCA, PCLer, PCimprimir, PCSe, PCEntao, PCEnquanto, PCInicio, PCFim, 
-	OPAritMult, OPAritDiv, OPAritSoma, OPAritSub, OPRelMenor, OPRelMenorIgual, OPRelMaiorIgual, OPRelMaior, OPRelIgual, OPRelDif, OPBoolE, OPBoolOu, 
-	DELim, ABrePar, FechaPar, Var, NumInt, NumReal, Cadeia, Fim
-}
--- tentativas_compilador/compilador/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-	public static void main(String[] args) {
-		AlgumaLexico lex = new AlgumaLexico(args[0]);
-        Token t = null;
-        while((t = lex.proximoToken()) != null) {
-            System.out.print(t);
-        }
-	}
-}
