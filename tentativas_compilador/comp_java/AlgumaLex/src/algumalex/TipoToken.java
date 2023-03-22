999fb53c239d477cf026e8c384317f36084062dc
Guilherme Bernardo <bguilherme51@gmail.com>
2023-03-21 23:21:39 -0400

    testes

--- tentativas_compilador/comp_java/AlgumaLex/src/algumalex/TipoToken.java
+++ tentativas_compilador/comp_java/AlgumaLex/src/algumalex/TipoToken.java
@@ -9,11 +9,27 @@ package algumalex;
  * @author Guilherme
  */
 public enum TipoToken {
-    PCDeclaracoes, PCAlgoritmo, PCInteiro, PCReal, PCAtribuir, PCA, PCLer,
-    PCImprimir, PCSe, PCEntao, PCEnquanto, PCInicio, PCFim,
-    OpAritMult, OpAritDiv, OpAritSoma, OpAritSub,
-    OpRelMenor, OpRelMenorIgual, OpRelMaiorIgual,
-    OpRelMaior, OpRelIgual, OpRelDif,
-    OpBoolE, OpBoolOu,
-    Delim, AbrePar, FechaPar, Var, NumInt, NumReal, Cadeia, Fim
+    PCDeclaracoes/**/, PCAlgoritmo/**/, 
+
+    PCInteiro/**/, PCReal/**/,
+
+    PCAtribuir/**/, PCA/**/, PCLer/**/, 
+    PCImprimir/**/, PCSe/**/, PCEntao/**/, 
+    PCEnquanto/**/,
+
+    PCInicio/**/, PCFim/**/,
+
+    OpAritMult/**/, OpAritDiv/**/, OpAritSoma/**/,OpAritSub/**/,
+
+    OpRelMenor/**/, OpRelMenorIgual/**/, OpRelMaiorIgual/**/,
+    OpRelMaior/**/, OpRelIgual/**/, OpRelDif/**/,
+
+
+    OpBoolE/**/, OpBoolOu/**/,
+
+    Delim/**/, 
+    
+    AbrePar/**/, FechaPar/**/, 
+    
+    Var/**/, NumInt/**/, NumReal/**/, Cadeia/**/, Fim/**/
 } //enum para identificar os tipos de Tokens
